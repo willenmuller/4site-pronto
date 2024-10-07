e39bfe92f46ef937a7bba8eb0408db2201f3a667
willenmuller <159459161+willenmuller@users.noreply.github.com>
2024-10-07 12:15:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Historia',
+    'Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?',
+    'Marie Curie'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'Quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118 elementos'
+)
+criaCartao(
+    'Fisica',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo (m/s)'
+)
+criaCartao(
+    'Ancestralidade',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico'
+)
